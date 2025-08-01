9b166de29d62e33d5617cfc0a2f822e7d3bc3b75
Parneet Kaur <you@example.com>
2025-08-01 14:35:25 +0530

    mis

--- src/config/url.js
+++ src/config/url.js
@@ -1,3 +1,3 @@
-// const server_url = "https://curemed-server.onrender.com";
-const server_url = "http://localhost:2003";
+const server_url = "https://curemed-server.onrender.com";
+// const server_url = "http://localhost:2003";
 export default server_url;
